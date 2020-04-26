143902da9b6bca3ff84e3fb9d431b23b1eb27f0a
coding-eggs <2665151959@qq.com>
2020-04-26 09:20:57 +0800

    s

--- cloud-discovery/src/main/java/com/cloud/blog/discovery/CloudDiscoveryApplication.java
+++ cloud-discovery/src/main/java/com/cloud/blog/discovery/CloudDiscoveryApplication.java
@@ -1,13 +1,11 @@
 package com.cloud.blog.discovery;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -24,6 +22,5 @@ public class CloudDiscoveryApplication extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         super.configure(http);
-        System.out.println();
     }
 }
